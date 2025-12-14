3cefbaac5fc23fccc5a81c17f372a5f1e59567d0
Rifat Mullayanov <34332137+rifat0mu@users.noreply.github.com>
2025-12-14 04:02:42 +0300

    Update killsboard size and column widths for improved layout in TeamKills mod
    - Reduced killsboard width for better alignment and presentation of player data.
    - Adjusted player column widths to enhance clarity and accommodate additional information.
    - Ensured consistent styling across the killsboard layout for improved visual consistency.

--- TeamKills/scripts/mods/TeamKills/killsboard/killsboard_view_settings.lua
+++ TeamKills/scripts/mods/TeamKills/killsboard/killsboard_view_settings.lua
@@ -2,13 +2,13 @@ local mod = get_mod("TeamKills")
 
 local killsboard_view_settings = {
     shading_environment = "content/shading_environments/ui/system_menu",
-    killsboard_size = {1300, 900},
+    killsboard_size = {1200, 900},
     killsboard_row_height = 16, -- 20
     killsboard_row_header_height = 22, -- 30
     killsboard_column_width = 110, -- ширина для K или D столбца
-    killsboard_column_player_width = 240, -- ширина для пары K+D (2 столбца) - используется для позиционирования
-    killsboard_column_player_bg_width = 280, -- ширина фона столбца игрока
-    killsboard_column_header_width = 210,
+    killsboard_column_player_width = 220, -- ширина для пары K+D (2 столбца) - используется для позиционирования
+    killsboard_column_player_bg_width = 220, -- ширина фона столбца игрока
+    killsboard_column_header_width = 220,
     killsboard_fade_length = 0.1,
     killsboard_font_size = 13, -- размер шрифта для обычных строк
     killsboard_font_size_header = 13, -- размер шрифта для заголовка
