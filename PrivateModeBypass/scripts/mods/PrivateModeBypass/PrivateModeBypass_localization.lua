c76cf8ffceb406f4d2801de728d2af2e199a2543
Rifat Mullayanov <34332137+rifat0mu@users.noreply.github.com>
2025-12-21 13:21:00 +0300

    Обновлена локализация в PrivateModeBypass: добавлены английские версии для названия мода, описания и заголовков, улучшена структура локализации для поддержки многоязычности.

--- PrivateModeBypass/scripts/mods/PrivateModeBypass/PrivateModeBypass_localization.lua
+++ PrivateModeBypass/scripts/mods/PrivateModeBypass/PrivateModeBypass_localization.lua
@@ -1,8 +1,19 @@
 return {
-	mod_name = "Private Mode Bypass",
-	mod_description = "Позволяет войти в приватный режим без пати. Обходит блокировку разработчиков.",
-	
-	mod_private_mode_bypass_enabled_title = "Включить обход блокировки",
-	mod_private_mode_bypass_enabled_description = "Включить обход проверки наличия участников пати для приватного режима",
+	mod_name = {
+		en = "Private Mode Bypass",
+		ru = "Обход приватного режима",
+	},
+	mod_description = {
+		en = "Allows entering private mode without a party. Bypasses the developer's restriction.",
+		ru = "Позволяет войти в приватный режим без пати. Обходит блокировку разработчиков.",
+	},
+	mod_private_mode_bypass_enabled_title = {
+		en = "Enable Bypass",
+		ru = "Включить обход блокировки",
+	},
+	mod_private_mode_bypass_enabled_description = {
+		en = "Enable bypass of the party member requirement check for private mode.",
+		ru = "Включить обход проверки наличия участников пати для приватного режима",
+	},
 }
 
