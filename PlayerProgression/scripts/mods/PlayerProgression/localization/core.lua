03c0c21ad2506892bb158af28b58d47ab25e4d9e
Rifat Mullayanov <34332137+rifat0mu@users.noreply.github.com>
2026-01-10 00:34:30 +0300

    Add keybindings and controller settings

--- PlayerProgression/scripts/mods/PlayerProgression/localization/core.lua
+++ PlayerProgression/scripts/mods/PlayerProgression/localization/core.lua
@@ -35,5 +35,177 @@ return {
 		en = "Select items in statistics to display here",
 		ru = "Выберите элементы в статистике для отображения здесь",
 	},
+	i18n_keybindings_section = {
+		en = "Keybindings",
+		ru = "Привязки клавиш",
+	},
+	i18n_hotkeys_section = {
+		en = "Hotkeys",
+		ru = "Горячие клавиши",
+	},
+	i18n_controller_section = {
+		en = "Controller",
+		ru = "Контроллер",
+	},
+	i18n_playstation_section = {
+		en = "Sony Playstation",
+		ru = "Sony Playstation",
+	},
+	playstation_controller_button = {
+		en = "Toggle Stats Display",
+		ru = "Переключить отображение статистики",
+	},
+	playstation_controller_button_default = {
+		en = "R1 (Default)",
+		ru = "R1 (По умолчанию)",
+	},
+	playstation_controller_button_l1 = {
+		en = "L1",
+		ru = "L1",
+	},
+	playstation_controller_button_l2 = {
+		en = "L2",
+		ru = "L2",
+	},
+	playstation_controller_button_l3 = {
+		en = "L3",
+		ru = "L3",
+	},
+	playstation_controller_button_r1 = {
+		en = "R1",
+		ru = "R1",
+	},
+	playstation_controller_button_r2 = {
+		en = "R2",
+		ru = "R2",
+	},
+	playstation_controller_button_r3 = {
+		en = "R3",
+		ru = "R3",
+	},
+	playstation_controller_button_triangle = {
+		en = "Triangle",
+		ru = "Треугольник",
+	},
+	playstation_controller_button_circle = {
+		en = "Circle",
+		ru = "Круг",
+	},
+	playstation_controller_button_cross = {
+		en = "Cross",
+		ru = "Крестик",
+	},
+	playstation_controller_button_square = {
+		en = "Square",
+		ru = "Квадрат",
+	},
+	playstation_controller_button_d_up = {
+		en = "D-Pad Up",
+		ru = "Крестовина Вверх",
+	},
+	playstation_controller_button_d_down = {
+		en = "D-Pad Down",
+		ru = "Крестовина Вниз",
+	},
+	playstation_controller_button_d_left = {
+		en = "D-Pad Left",
+		ru = "Крестовина Влево",
+	},
+	playstation_controller_button_d_right = {
+		en = "D-Pad Right",
+		ru = "Крестовина Вправо",
+	},
+	playstation_controller_button_options = {
+		en = "Options",
+		ru = "Options",
+	},
+	playstation_controller_button_touch = {
+		en = "Touchpad",
+		ru = "Тачпад",
+	},
+	playstation_controller_button_description = {
+		en = "Select the button on PlayStation controller to toggle stats display.",
+		ru = "Выберите кнопку на контроллере PlayStation для переключения отображения статистики.",
+	},
+	i18n_xbox_section = {
+		en = "Xbox",
+		ru = "Xbox",
+	},
+	xbox_controller_button = {
+		en = "Toggle Stats Display",
+		ru = "Переключить отображение статистики",
+	},
+	xbox_controller_button_default = {
+		en = "RB (Default)",
+		ru = "RB (По умолчанию)",
+	},
+	xbox_controller_button_left_shoulder = {
+		en = "LB",
+		ru = "LB",
+	},
+	xbox_controller_button_left_trigger = {
+		en = "LT",
+		ru = "LT",
+	},
+	xbox_controller_button_left_thumb = {
+		en = "LS",
+		ru = "LS",
+	},
+	xbox_controller_button_right_shoulder = {
+		en = "RB",
+		ru = "RB",
+	},
+	xbox_controller_button_right_trigger = {
+		en = "RT",
+		ru = "RT",
+	},
+	xbox_controller_button_right_thumb = {
+		en = "RS",
+		ru = "RS",
+	},
+	xbox_controller_button_y = {
+		en = "Y",
+		ru = "Y",
+	},
+	xbox_controller_button_x = {
+		en = "X",
+		ru = "X",
+	},
+	xbox_controller_button_a = {
+		en = "A",
+		ru = "A",
+	},
+	xbox_controller_button_b = {
+		en = "B",
+		ru = "B",
+	},
+	xbox_controller_button_d_up = {
+		en = "D-Pad Up",
+		ru = "Крестовина Вверх",
+	},
+	xbox_controller_button_d_down = {
+		en = "D-Pad Down",
+		ru = "Крестовина Вниз",
+	},
+	xbox_controller_button_d_left = {
+		en = "D-Pad Left",
+		ru = "Крестовина Влево",
+	},
+	xbox_controller_button_d_right = {
+		en = "D-Pad Right",
+		ru = "Крестовина Вправо",
+	},
+	xbox_controller_button_start = {
+		en = "Start",
+		ru = "Start",
+	},
+	xbox_controller_button_back = {
+		en = "Back",
+		ru = "Back",
+	},
+	xbox_controller_button_description = {
+		en = "Select the button on Xbox controller to toggle stats display.",
+		ru = "Выберите кнопку на контроллере Xbox для переключения отображения статистики.",
+	},
 }
 
